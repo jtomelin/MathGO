1f5d8f8dacfe9c389ff6be70c6d2163c3b21c67b
Jader Tomelin <jader.tomelin@gmail.com>
2018-10-12 11:41:51 -0300

    Iniciando o repositório

---
+++ Assets/SceneBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneBehavior : MonoBehaviour {
+
+    public Button btnCliqueAqui;
+    public GameObject obj;
+    public bool bActive;
+
+	// Use this for initialization
+	void Start () {
+        bActive = false;
+        obj.SetActive(bActive);
+        btnCliqueAqui.onClick.AddListener(eventoCliqueAqui);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        obj.transform.Rotate(Vector3.right * Time.deltaTime * 20);
+
+        obj.transform.Rotate(Vector3.up * Time.deltaTime * 20);
+    }
+
+    void eventoCliqueAqui() {
+
+        bActive = !bActive;
+
+        obj.SetActive(bActive);
+    }
+
+}
