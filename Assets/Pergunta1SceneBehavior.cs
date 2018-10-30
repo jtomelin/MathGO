4e0db3ccd6b83d3ce2f15db80bc498eb2fd90efe
Jader Tomelin <jader.tomelin@gmail.com>
2018-10-29 23:27:15 -0300

    Arrumando a baguncinha

---
+++ Assets/Pergunta1SceneBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pergunta1SceneBehavior : SceneBehaviorBase
+{
+    //public Button btnCliqueAqui;
+    public GameObject obj;
+    public bool bActive;
+
+    Pergunta1SceneBehavior()
+    {
+        SetRightAnswerButton(eButton.eButtonA);
+    }
+
+    // Use this for initialization
+    public override void Start ()
+    {
+        base.Start();
+        bActive = false;
+        obj.SetActive(bActive);
+	}
+	
+	// Update is called once per frame
+	public override void Update ()
+    {
+        obj.transform.Rotate(Vector3.right * Time.deltaTime * 100);
+        obj.transform.Rotate(Vector3.up * Time.deltaTime * 100);
+    }
+
+    void eventoCliqueAqui()
+    {
+        bActive = !bActive;
+        obj.SetActive(bActive);
+    }
+
+}
---
+++ Assets/SceneBehaviorBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SceneBehaviorBase : MonoBehaviour
+{
+    public enum eButton
+    {
+        eNotImpl = 0,
+        eButtonA    ,
+        eButtonB    ,
+        eButtonC    ,
+    }
+
+    public Image imageComponent;
+
+    private Sprite greenButton;
+
+    public Button btnAlternativaA;
+    public Button btnAlternativaB;
+    public Button btnAlternativaC;
+
+    private eButton rightAnswerButton;
+
+    // Use this for initialization
+    public virtual void Start()
+    {
+        greenButton = Resources.Load<Sprite>("botao-alternativa-correta");
+
+        imageComponent        = GetComponent<Image>();
+        imageComponent.sprite = greenButton;
+
+        btnAlternativaA.onClick.AddListener(OnButtonAClick);
+        btnAlternativaB.onClick.AddListener(OnButtonBClick);
+        btnAlternativaC.onClick.AddListener(OnButtonCClick);
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+    }
+
+    void OnGenericButtonClick(eButton buttonClicked)
+    {
+        if (buttonClicked == rightAnswerButton)
+        {
+            //do something
+        }
+        else
+        {
+            // do another thing
+        }
+    }
+
+    void OnButtonAClick() { OnGenericButtonClick(eButton.eButtonA); }
+    void OnButtonBClick() { OnGenericButtonClick(eButton.eButtonB); }
+    void OnButtonCClick() { OnGenericButtonClick(eButton.eButtonC); }
+
+    public void SetRightAnswerButton(eButton rightAnswerButton) { this.rightAnswerButton = rightAnswerButton; }
+}
